68f1d74ff18cda59d1b77cc15458d1aa58eb658a
Mike <mikewhittom27@gmail.com>
2026-02-26 16:37:11 -0500

    adding default .env

--- server/db/client.ts
+++ server/db/client.ts
@@ -4,8 +4,8 @@ import mysql from "mysql2/promise";
 import { schema } from "./schema.js";
 
 const databaseUrl =
-	process.env.DATABASE_URL ??
-	`mysql://${process.env.DATABASE_USERNAME ?? "root"}:${process.env.DATABASE_PASSWORD ?? ""}@${process.env.DATABASE_HOST ?? "127.0.0.1"}:${process.env.DATABASE_PORT ?? "3306"}/personal_assistant`;
+  process.env.DATABASE_URL ??
+  `mysql://${process.env.DATABASE_USERNAME ?? "root"}:${process.env.DATABASE_PASSWORD ?? ""}@${process.env.DATABASE_HOST ?? "127.0.0.1"}:${process.env.DATABASE_PORT ?? "3306"}/personal_assistant`;
 
 const queryClient = mysql.createPool(databaseUrl);
 
